0445d9e2776a436fede9c3573e34b90928fad25c
Douglas Egiemeh <douglasegiemeh@gmail.com>
2019-03-18 10:08:19 +0100

    initial commit

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const index = (props) => {
+        return (
+            <div>
+                <p>Home</p>
+            </div>
+        );
+}
+
+export default index;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const sell = (props) => {
+        return (
+            <div>
+                <p>Sell</p>
+        </div>
+        );
+}
+
+export default sell;
